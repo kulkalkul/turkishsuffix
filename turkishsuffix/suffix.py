d4a0fc6f89f9db041f8d597c697e9bc71280fdf9
Bora ÜLKER <yokunjon@gmail.com>
2018-11-11 00:21:04 +0300

    Suffix method can decide vowel harmony now. Used generator to find last vowel of the word.

--- turkishsuffix/suffix.py
+++ turkishsuffix/suffix.py
@@ -1,9 +1,23 @@
 from config_loader import config
 
-settings = config["settings"]
-exceptions = config["exceptions"]
+import collections
+
+Settings = config["settings"]
+Exceptions = config["exceptions"]
 
 
 class TurkishSuffix:
     def __init__(self):
-        pass
+        self.__rule_set = dict(zip(Settings.types, Settings.rule_set))
+
+    def suffix(self, word, suffix_type):
+        index, division, first, last, *options = self.__rule_set[suffix_type]
+        index, division = int(index), int(division)
+
+        vowel_from_backwards = (letter for letter in word[::-1] if letter in Settings.vowels)
+        last_vowel = vowel_from_backwards.__next__()
+
+        vowel_harmony = index + Settings.vowels.index(last_vowel) // 2 % division
+        vowel = Settings.suffix_vowels[vowel_harmony]
+
+        return vowel
